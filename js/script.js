24e35ca460b6be5629ac0856c641713a6e7448f7
agungkurniawan956 <kurniawanagung956@gmail.com>
2023-12-26 22:03:49 +0700

    add modal product, shoping cart, shearch form and modal product

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
-// toggle class active
+// toggle hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 
 document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
+};
+
+// toggle search button
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.getElementById('search-box');
+const searchButton = document.getElementById('search-button');
+
+searchButton.onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+// Shopping cart
+const btnCart = document.getElementById('btn-shopping-cart');
+const cartForm = document.querySelector('.cart-form');
+
+btnCart.onclick = (e) => {
+  cartForm.classList.toggle('active');
+  e.preventDefault();
 };
 
 // menutup sidebar
@@ -12,4 +32,38 @@ document.addEventListener('click', function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
   }
+
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
+  }
+
+  if (!btnCart.contains(e.target) && !cartForm.contains(e.target)) {
+    cartForm.classList.remove('active');
+  }
 });
+
+// Modal Product
+const modalProduct = document.getElementById('modal-product');
+const btnDetailProducts = document.querySelectorAll('.btn-detail-product');
+
+btnDetailProducts.forEach((btn) => {
+  btn.onclick = (e) => {
+    modalProduct.classList.add('active');
+    e.preventDefault();
+  };
+});
+
+const btnCloseModal = document.querySelector(
+  '.modal-container .btn-close-modal'
+);
+
+btnCloseModal.onclick = (e) => {
+  modalProduct.classList.remove('active');
+  e.preventDefault();
+};
+
+window.onclick = (e) => {
+  if (e.target === modalProduct) {
+    modalProduct.classList.remove('active');
+  }
+};
